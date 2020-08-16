548aba4cb9721cbb97fb0fd80adf609eb9dd2fef
Alex Pearson <alex@alexpear.com>
2020-08-16 11:06:10 -0400

    🏁 Handle routing with express

--- server/index.js
+++ server/index.js
@@ -1,128 +1,66 @@
-const http = require("http");
+const bodyParser = require("body-parser");
+const express = require("express");
 const path = require("path");
 const FileSync = require("lowdb/adapters/FileSync");
 const lodashId = require("lodash-id");
 const lowdb = require("lowdb");
+const morgan = require("morgan");
+
+const app = express();
 const adapter = new FileSync(path.join(__dirname, "db.json"));
 const db = lowdb(adapter);
 
 db._.mixin(lodashId);
 db.defaults({ posts: [] }).write();
 
-const PORT = 8675;
+const PORT = process.env.PORT || 8675;
 
-const HANDLERS = {
-  GET(request, response) {
-    const posts = db.get("posts");
+app.use(bodyParser.json()).use(morgan("dev"));
 
-    if (request.url === "/posts") {
-      ok(response, { posts: posts.value() });
+app
+  .route("/posts")
+  .get((_request, response) => {
+    const posts = db.get("posts").value();
+
+    response.json(posts);
+  })
+  .post((request, response) => {
+    const post = db.get("posts").insert(request.body).write();
+
+    response.json(post);
+  });
+
+app
+  .route("/posts/:id")
+  .get((request, response) => {
+    const id = request.params.id;
+    const post = db.get("posts").getById(id).value();
+
+    if (post) {
+      response.json(post);
     } else {
-      const parts = request.url.split("/");
-
-      if (parts.length === 3) {
-        const id = parts[2];
-        const post = posts.getById(id).value();
-
-        if (post) {
-          ok(response, post);
-        } else {
-          notFound(response);
-        }
-      } else {
-        notFound(response);
-      }
+      response.status(404).json({ message: "Not Found" });
     }
-  },
-  POST(request, response) {
-    let contents = "";
+  })
+  .patch((request, response) => {
+    const id = request.params.id;
+    const post = db.get("posts").updateById(id, request.body).write();
 
-    request.on("data", (chunk) => (contents += chunk));
-
-    request.on("end", () => {
-      const post = db.get("posts").insert({ body: contents }).write();
-
-      ok(response, post);
-    });
-
-    request.on("error", () => internalServerError(response));
-  },
-  PATCH(request, response) {
-    const parts = request.url.split("/");
-
-    if (parts.length === 3) {
-      let contents = "";
-
-      request.on("data", (chunk) => (contents += chunk));
-
-      request.on("end", () => {
-        const id = parts[2];
-        const post = db.get("posts").updateById(id, { body: contents }).write();
-
-        if (post) {
-          ok(response, post);
-        } else {
-          notFound(response);
-        }
-      });
-
-      request.on("error", () => internalServerError(response));
+    if (post) {
+      response.json(post);
     } else {
-      notFound(response);
+      response.status(404).json({ message: "Not Found" });
     }
-  },
-  DELETE(request, response) {
-    const parts = request.url.split("/");
+  })
+  .delete((request, response) => {
+    const id = request.params.id;
+    const post = db.get("posts").removeById(id).write();
 
-    if (parts.length === 3) {
-      const id = parts[2];
-      const post = db.get("posts").removeById(id).write();
-
-      if (post) {
-        ok(response, post);
-      } else {
-        notFound(response);
-      }
+    if (post) {
+      response.json(post);
     } else {
-      notFound(response);
+      response.status(404).json({ message: "Not Found" });
     }
-  },
-};
+  });
 
-const notFound = (response) => {
-  response.writeHead(404);
-  response.write(JSON.stringify({ message: "Not Found" }));
-  response.end();
-};
-
-const internalServerError = (response) => {
-  response.writeHead(500);
-  response.write(JSON.stringify({ message: "Internal Server Error" }));
-  response.end();
-};
-
-const ok = (response, payload) => {
-  response.writeHead(200);
-  response.write(JSON.stringify(payload));
-  response.end();
-};
-
-const server = http.createServer((request, response) => {
-  response.setHeader("Content-Type", "application/json");
-
-  if (request.url.startsWith("/posts")) {
-    const handler = HANDLERS[request.method];
-
-    if (handler) {
-      handler(request, response);
-    } else {
-      notFound(response);
-    }
-  } else {
-    notFound(response);
-  }
-});
-
-server.listen(PORT);
-
-console.log(`Listening on port ${PORT}`);
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
